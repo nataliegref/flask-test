6ec68385649db26c5709e8ceec0cb5ab4b1e5a64
Natalie Grefenstette <nataliegref@gmail.com>
2024-06-18 15:23:37 +0200

    Add /model endpoint

--- api/index.py
+++ api/index.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Hello, World!'
+
+@app.route("/model", methods=['GET'])
+def return_home():
+    average = 20
+    return jsonify({
+        'message': "Hello! "+str(average)
+    })
 
 @app.route('/about')
 def about():
